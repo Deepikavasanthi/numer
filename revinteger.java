85bf1eb996d4eab1a61da6ab2774a5dad51358e1
Deepikavasanthi <deepikamailtome@gmail.com>
2016-07-16 03:13:30 -0700

    Add files via upload

---
+++ revinteger.java
@@ -0,0 +1,14 @@
+int n1,n2, rev=0;  rev=0;
+        System.out.println("enter the number ");
+         Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+       while(n!=0)
+        {           
+             n2=n%10;
+             rev=rev*10+n2;
+             n1=n/10;
+             n=n1; 
+        }
+            System.out.println("the reverse no is   "+rev);
+        
+         }
