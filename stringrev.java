f0e7fb636bf36ed9f8baa783e4d3374acbb0b9cb
Deepikavasanthi <deepikamailtome@gmail.com>
2016-07-16 02:31:03 -0700

    Add files via upload

---
+++ stringrev.java
@@ -0,0 +1,5 @@
+Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        String b=new StringBuffer(a).reverse().toString();
+        System.out.println("the reverse string is  "   +b);
+      
