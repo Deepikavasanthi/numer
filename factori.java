76e03452513f63633d9e9e74dcfa131d391b9c9a
Deepikavasanthi <deepikamailtome@gmail.com>
2016-07-16 02:47:14 -0700

    Add files via upload

---
+++ factori.java
@@ -0,0 +1,10 @@
+int fact=1,i;
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(i=1;i<=n;i++)
+        {
+            fact=fact*i;
+         }
+        {
+            System.out.println("fact="+fact);
+         }
